d5293c96221e24ce2e9d38fd050008e913564420
Igor Borenovic <igorborenovic4@gmail.com>
2020-05-01 13:05:59 +0200

    Add validations

--- index.php
+++ index.php
@@ -11,20 +11,56 @@ $klein = new \Klein\Klein();
 $routeConfig = Yaml::parse(file_get_contents(CONFIG_PATH));
 
 foreach ($routeConfig['routes'] as $currentConfig) {
+    validateClass($currentConfig['class']);
+    validateMethod($currentConfig['class'], $currentConfig['function']);
+
     $reflectionClass  = new ReflectionClass($currentConfig['class']);
     $reflectionMethod = $reflectionClass->getMethod($currentConfig['function']);
 
-    $arguments = [];
-
-    foreach ($currentConfig['dependency'] ?? [] as $dependency) {
-        $arguments[] = new $dependency;
-    }
 
     $klein->respond(
         $currentConfig['method'],
         $currentConfig['path'],
-        $reflectionMethod->getClosure($reflectionClass->newInstanceArgs($arguments))
+        $reflectionMethod->getClosure(
+            $reflectionClass->newInstanceArgs(constructArguments($currentConfig['dependency'] ?? []))
+        )
     );
 }
 
 $klein->dispatch();
+
+function constructArguments(array $dependencies): array
+{
+    $arguments = [];
+
+    foreach ($dependencies as $dependency) {
+        validateClass($dependency);
+
+        $arguments[] = new $dependency;
+    }
+
+    return $arguments;
+}
+
+/**
+ * @param string $className
+ */
+function validateClass(string $className): void
+{
+    if (!class_exists($className)) {
+        throw new RuntimeException(sprintf('Class %s does not exist!', $className));
+    }
+}
+
+/**
+ * @param string $className
+ * @param string $methodName
+ */
+function validateMethod(string $className, string $methodName): void
+{
+    $class = (new ReflectionClass($className))->newInstanceWithoutConstructor();
+
+    if (!method_exists($class, $methodName)) {
+        throw new RuntimeException(sprintf('Method %s does not exist!', $methodName));
+    }
+}
