1a0089eda9dcab10f9dc02c6574801ae233cc65f
Igor Borenovic <igorborenovic4@gmail.com>
2020-05-01 12:09:55 +0200

    Remove unused line, fix code alignment

--- index.php
+++ index.php
@@ -11,11 +11,13 @@ $klein = new \Klein\Klein();
 $routeConfig = Yaml::parse(file_get_contents(CONFIG_PATH));
 
 foreach ($routeConfig['routes'] as $currentConfig) {
-    $class = new $currentConfig['class'];
-
     $reflectionMethod = new ReflectionMethod($currentConfig['class'], $currentConfig['function']);
 
-    $klein->respond($currentConfig['method'], $currentConfig['path'], $reflectionMethod->getClosure(new $currentConfig['class']));
+    $klein->respond(
+        $currentConfig['method'],
+        $currentConfig['path'],
+        $reflectionMethod->getClosure(new $currentConfig['class'])
+    );
 }
 
 $klein->dispatch();
